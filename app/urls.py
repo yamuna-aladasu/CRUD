56d7c7c8b3b98baf93ab776382655adb64f9138d
yamuna aladasu <yamuna.aladasu@bigthinkcode.com>
2026-04-17 12:59:48 +0530

    modelviewset

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,15 @@
-from django.urls import path
-from .views import Createlist,Productupdate,Productdeletion,Productpartialupdate
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from .views import Createlist,Productupdate,Productdeletion,Productpartialupdate,Productview
+router = DefaultRouter()
+router.register('product', Productview)
 urlpatterns=[
     path('productcreated/',Createlist.as_view(),name='productcreation'),
     path('products/',Createlist.as_view(),name='productdisplay'),
     path('updateproduct/<int:pk>/',Productupdate.as_view(),name='updation'),
     path('productdelete/<int:pk>/',Productdeletion.as_view(),name='deletion'),
-    path('productpatch/<int:pk>/',Productpartialupdate.as_view(),name='partial')
+    path('productpatch/<int:pk>/',Productpartialupdate.as_view(),name='partial'),
+    path('', include(router.urls)),
 ]
+
 
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.parsers import MultiPartParser, FormParser
 from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
@@ -50,4 +51,6 @@ class Productpartialupdate(APIView):
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
     
-
+class Productview(ModelViewSet):
+    queryset=Product.objects.all()
+    serializer_class=Productserializer
